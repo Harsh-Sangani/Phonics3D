dd5568f836e0704fe619c074457c87bb6c5d586b
Harsh-Sangani <harshsangani38@gmail.com>
2024-03-06 01:54:38 -0600

    added my Phonics3D web app

---
+++ scripts/btnclickevents.js
@@ -0,0 +1,193 @@
+let abtn = document.getElementById("a");
+let bbtn = document.getElementById("b");
+let cbtn = document.getElementById("c");
+let dbtn = document.getElementById("d");
+let ebtn = document.getElementById("e");
+let fbtn = document.getElementById("f");
+let gbtn = document.getElementById("g");
+let hbtn = document.getElementById("h");
+let ibtn = document.getElementById("i");
+let jbtn = document.getElementById("j");
+let kbtn = document.getElementById("k");
+let lbtn = document.getElementById("l");
+let mbtn = document.getElementById("m");
+let nbtn = document.getElementById("n");
+let obtn = document.getElementById("o");
+let pbtn = document.getElementById("p");
+let qbtn = document.getElementById("q");
+let rbtn = document.getElementById("r");
+let sbtn = document.getElementById("s");
+let tbtn = document.getElementById("t");
+let ubtn = document.getElementById("u");
+let vbtn = document.getElementById("v");
+let wbtn = document.getElementById("w");
+let xbtn = document.getElementById("x");
+let ybtn = document.getElementById("y");
+let zbtn = document.getElementById("z");
+
+// abtn.addEventListener("click", function(e) {
+// window.location.href = ("apple.html");
+// });
+
+abtn.addEventListener("click", myFunction1);
+
+function myFunction1() {
+    window.location.href = "apple.html";
+}
+
+bbtn.addEventListener("click", myFunction2);
+
+function myFunction2() {
+    window.location.href = "boat.html";
+}
+
+cbtn.addEventListener("click", myFunction3);
+
+function myFunction3() {
+    window.location.href = "car.html";
+}
+
+dbtn.addEventListener("click", myFunction4);
+
+function myFunction4() {
+    window.location.href = "dog.html";
+}
+
+ebtn.addEventListener("click", myFunction5);
+
+function myFunction5() {
+    window.location.href = "elephant.html";
+}
+
+fbtn.addEventListener("click", myFunction6);
+
+function myFunction6() {
+    window.location.href = "flower.html";
+}
+
+gbtn.addEventListener("click", myFunction7);
+
+function myFunction7() {
+    window.location.href = "grapes.html";
+}
+
+hbtn.addEventListener("click", myFunction8);
+
+function myFunction8() {
+    window.location.href = "house.html";
+}
+
+ibtn.addEventListener("click", myFunction9);
+
+function myFunction9() {
+    window.location.href = "iphone.html";
+}
+
+jbtn.addEventListener("click", myFunction10);
+
+function myFunction10() {
+    window.location.href = "jet.html";
+}
+
+kbtn.addEventListener("click", myFunction11);
+
+function myFunction11() {
+    window.location.href = "key.html";
+}
+
+lbtn.addEventListener("click", myFunction12);
+
+function myFunction12() {
+    window.location.href = "lamp.html";
+}
+
+mbtn.addEventListener("click", myFunction13);
+
+function myFunction13() {
+    window.location.href = "moon.html";
+}
+
+nbtn.addEventListener("click", myFunction14);
+
+function myFunction14() {
+    window.location.href = "nest.html";
+}
+
+obtn.addEventListener("click", myFunction15);
+
+function myFunction15() {
+    window.location.href = "octopus.html";
+}
+
+pbtn.addEventListener("click", myFunction16);
+
+function myFunction16() {
+    window.location.href = "pencil.html";
+}
+
+qbtn.addEventListener("click", myFunction17);
+
+function myFunction17() {
+    window.location.href = "Q.html";
+}
+
+
+rbtn.addEventListener("click", myFunction18);
+
+function myFunction18() {
+    window.location.href = "rat.html";
+}
+
+
+sbtn.addEventListener("click", myFunction19);
+
+function myFunction19() {
+    window.location.href = "shark.html";
+}
+
+
+tbtn.addEventListener("click", myFunction20);
+
+function myFunction20() {
+    window.location.href = "train.html";
+}
+
+
+ubtn.addEventListener("click", myFunction21);
+
+function myFunction21() {
+    window.location.href = "umbrella.html";
+}
+
+vbtn.addEventListener("click", myFunction22);
+
+function myFunction22() {
+    window.location.href = "violin.html";
+}
+
+wbtn.addEventListener("click", myFunction23);
+
+function myFunction23() {
+    window.location.href = "watermelon.html";
+}
+
+xbtn.addEventListener("click", myFunction24);
+
+function myFunction24() {
+    window.location.href = "xmas.html";
+}
+
+ybtn.addEventListener("click", myFunction25);
+
+function myFunction25() {
+    window.location.href = "yolk.html";
+}
+
+
+
+
+
+
+
+
+
